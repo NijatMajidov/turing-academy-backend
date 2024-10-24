34aa83ed2a3bcabdd4c70cdb532eb5158602a9ad
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-10-24 20:08:49 +0400

    print, println, printf

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        System.out.print("Hello, World");
+        System.out.println("\nHello, World");
+        System.out.printf("Hello,%s"," World\n");
+        System.out.printf("Hello, World: %.2f\n", 50.0);
+        System.out.printf("Hello, World: %f", 50.0);
+        System.out.printf("\nHello, World: %d", 10);
+        System.out.printf("\nHello,%s -%s"," World","2324");
     }
 }
