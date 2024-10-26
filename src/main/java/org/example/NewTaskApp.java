030d8778b4440338be49f30dfac00f5861dec64c
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-10-26 18:45:30 +0400

    New task

---
+++ src/main/java/org/example/NewTaskApp.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class NewTaskApp {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number: \t");
+        int number = scan.nextInt();
+        System.out.println("Number is "+number);
+        System.out.print("Cutdurse true, tekdirse false: \t");
+                System.out.print( number % 2 == 0 && true || false);
+    }
+}
