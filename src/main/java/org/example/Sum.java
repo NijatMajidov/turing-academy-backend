d33a0c2bb4b3c4f3e31b2207558a1d897e0a3dbe
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-10-26 18:22:51 +0400

    Update

--- src/main/java/org/example/Sum.java
+++ src/main/java/org/example/Sum.java
@@ -9,13 +9,16 @@ public class Sum {
        int sum = number1+number2;
        int ferq = number1-number2;
        int bol = number1/number2;
+       float kesr = (float) number1/number2;
        int vur = number1*number2;
        int qaliq = number1%number2;
        int comment = number1;//number2 bu komentdir
 
-        System.out.println(sum);
-        System.out.println(ferq);
-        System.out.println(bol);
+        System.out.printf("Cem: %d\n",sum);
+        System.out.printf("ferq: %d\n",ferq);
+        System.out.printf("tam: %d\n",bol);
+        System.out.printf("kesr hisse: %f\n", kesr-bol);
+        System.out.println(kesr);
         System.out.println(vur);
         System.out.println(qaliq);
 
