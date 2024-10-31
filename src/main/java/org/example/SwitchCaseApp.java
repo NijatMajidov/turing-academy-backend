e0c18c45307b307edb9bcd9319f8ed5f3d8c71d4
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-10-31 20:01:42 +0400

    Switch case

---
+++ src/main/java/org/example/SwitchCaseApp.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class SwitchCaseApp {
+    public static void main(String[] args) {
+        //input
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Eded daxil edin : ");
+        int value= scan.nextInt();
+        String result = "";
+
+        //proses
+        switch (value%3){
+            case 0:
+                result = "Eded 3-e tam bolunur";
+                break;
+            case 1:
+                result = "Eded 3-e tam bolunmur ve  qaliq 1-dir";
+                break;
+            case 2:
+                result = "Eded 3-e tam bolunmur ve  qaliq 2-dir";
+                break;
+            default:
+                result = "Eded 3-e tam bolunmur ve qaliq "+ value%3+"-dur";
+        }
+
+        //output
+        System.out.println(result);
+
+    }
+}
