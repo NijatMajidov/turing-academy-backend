cfcba3922eefb06b2cad33560f3ef3d3c3f4f7e9
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-10-31 18:58:32 +0400

    new commit

--- src/main/java/org/example/OperatorsApp.java
+++ src/main/java/org/example/OperatorsApp.java
@@ -14,7 +14,6 @@ public class OperatorsApp {
         // Unary operators
         System.out.println("a++: " + (a++));
         System.out.println("++a : " + (++a));
-
         System.out.println("b-- : " + (b--));
         System.out.println("--b : " + (--b));
 
@@ -35,11 +34,10 @@ public class OperatorsApp {
         System.out.println("num >>= 1: " + (num >>= 1));
         System.out.println("num >>>= 1: " + (num >> 1));
 
-        // Comparison operators
         System.out.print("Enter a number c: \t");
         int c = scan.nextInt();
         System.out.println("First number -" + a + " Second number -" + b + " third number -" + c);
-
+        // Comparison operators
         System.out.println("a > b: " + (a > b));
         System.out.println("a < b: " + (a < b));
         System.out.println("a >= b: " + (a >= b));
