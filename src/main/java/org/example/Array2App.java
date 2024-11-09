a8c68f534412e2c93c24e8e68c00e96180bb99e6
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-11-09 15:16:25 +0400

    New Task Codes

---
+++ src/main/java/org/example/Array2App.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Array2App {
+
+    public static void main(String[] args) {
+        //input
+        int arr[][] = new int[3][4];
+        Scanner scan = new Scanner(System.in);
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                arr[i][j]= scan.nextInt();
+            }
+        }
+
+        System.out.println(Arrays.deepToString(arr));
+        /*
+        for(int i=0;i<3;i++){
+            for(int j=0;j<4;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }*/
+    }
+}
---
+++ src/main/java/org/example/StringTask.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class StringTask {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter a word: ");
+            String word = scanner.next();
+            // 1. Insert a word and print the last character
+            char lastChar = word.charAt(word.length() - 1);
+            System.out.println("Last character: " + lastChar);
+
+            // 2. Insert a word and check if the last character is a vowel
+            if ("AEIOUaeiou".indexOf(lastChar) != -1) {
+                System.out.println("The last character is a vowel.");
+            } else {
+                System.out.println("The last character is not a vowel.");
+            }
+
+            // 3. Insert a word, reverse it, and convert it to uppercase
+            String reversedWord = new StringBuilder(word).reverse().toString().toUpperCase();
+            System.out.println("Reversed and uppercased word: " + reversedWord);
+        }
+}
+
+
+
