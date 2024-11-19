49a2182ca7ccac32c2ca588418d18c0fed0186bf
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-11-19 20:11:26 +0400

    Digits

--- src/main/java/backend/eolympProblems/problem2/DigitsApp.java
+++ src/main/java/backend/eolympProblems/problem2/DigitsApp.java
@@ -1,2 +1,22 @@
-package backend.eolympProblems.problem2;public class DigitsApp {
+package backend.eolympProblems.problem2;
+
+import java.util.Scanner;
+
+public class DigitsApp {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int count=0;
+
+        if(n==0){
+            count = 1;
+        }
+        while(n!=0){
+                count++;
+                n/=10;
+        }
+
+        System.out.print(count);
+    }
 }
