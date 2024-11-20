fc17c5e3bb252feb1293297dca33d202f6669bec
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-11-20 20:14:58 +0400

    BigAccurary

--- src/main/java/backend/eolympProblems/problem11/BigAccuracyApp.java
+++ src/main/java/backend/eolympProblems/problem11/BigAccuracyApp.java
@@ -1,2 +1,26 @@
-package backend.eolympProblems.problem11;public class BigAccuracyApp {
+package backend.eolympProblems.problem11;
+
+import java.util.Scanner;
+
+public class BigAccuracyApp {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        System.out.print(m/n);
+        int qaliq = m%n;
+        m= qaliq*10;
+        if(k>0){
+            System.out.print(".");
+        }
+        for (int i = 1; i <= k; i++)
+        {
+            System.out.print(m/n);
+            qaliq = m%n;
+            m = qaliq*10;
+        }
+    }
 }
