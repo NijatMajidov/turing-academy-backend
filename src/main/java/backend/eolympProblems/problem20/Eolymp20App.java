d04273b1787740ff27a5c6887aa4f9829d8b1d8d
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-11-21 00:21:15 +0400

    Problem 20

--- src/main/java/backend/eolympProblems/problem20/Eolymp20App.java
+++ src/main/java/backend/eolympProblems/problem20/Eolymp20App.java
@@ -1,2 +1,25 @@
-package backend.eolympProblems.problem20;public class Eolymp20App {
+package backend.eolympProblems.problem20;
+
+import java.util.Scanner;
+
+public class Eolymp20App {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long num = scanner.nextLong();
+        long temp,count=0;
+        long sum=0;
+        temp=num;
+        while(num>0) {
+            while(temp>0) {
+                sum+=temp%10;
+                temp/=10;
+            }
+            num-=sum;
+            count++;
+            temp=num;
+            sum=0;
+        }
+        System.out.print(count);
+    }
 }
