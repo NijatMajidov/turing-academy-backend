91afb41deb538c37737cb9018f63c36cc4c2a232
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-11-20 20:13:51 +0400

    Gardener

--- src/main/java/backend/eolympProblems/problem10/GardenerApp.java
+++ src/main/java/backend/eolympProblems/problem10/GardenerApp.java
@@ -1,2 +1,22 @@
-package backend.eolympProblems.problem10;public class GardenerApp {
+package backend.eolympProblems.problem10;
+
+import java.util.Scanner;
+
+public class GardenerApp {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            int n = scanner.nextInt();
+            double totalWater = 1.0;
+            int daysSkipped = 0;
+            int count = n;
+
+            while (totalWater >= 0.5) {
+                totalWater -= 1.0 / count;
+                daysSkipped++;
+                count--;
+            }
+
+            System.out.println(n - daysSkipped + 1);
+        }
 }
