35c495d51d4e080ca39ec9110e57bec439d08e4d
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-11-20 23:19:22 +0400

    GardenerPainter

--- src/main/java/backend/eolympProblems/problem17/GardenerPainterApp.java
+++ src/main/java/backend/eolympProblems/problem17/GardenerPainterApp.java
@@ -1,2 +1,17 @@
-package backend.eolympProblems.problem17;public class GardenerPainterApp {
+package backend.eolympProblems.problem17;
+
+import java.util.Scanner;
+
+public class GardenerPainterApp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int countTrees = sc.nextInt();
+
+        // Rəngləmə üsullarının sayını hesablayırıq
+        long netice = 3 * (long) Math.pow(2, countTrees - 1);
+
+        // Nəticəni ekrana yazdırırıq
+        System.out.println(netice);
+    }
 }
