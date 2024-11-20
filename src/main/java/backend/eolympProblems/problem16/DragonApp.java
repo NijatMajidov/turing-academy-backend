e2cc714b95c7ba7257becc79e79fe2b844033f9a
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-11-20 22:18:04 +0400

    Dragon legs

--- src/main/java/backend/eolympProblems/problem16/DragonApp.java
+++ src/main/java/backend/eolympProblems/problem16/DragonApp.java
@@ -1,2 +1,34 @@
-package backend.eolympProblems.problem16;public class DragonApp {
+package backend.eolympProblems.problem16;
+
+import java.util.Scanner;
+
+public class DragonApp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int legS = sc.nextInt();
+        int headDragon = sc.nextInt();
+        int totalHeads = sc.nextInt();
+        int totalLegs = sc.nextInt();
+        int countDragon, legsDragon;
+
+        if(legS * totalHeads == totalLegs){
+            System.out.print(legS * headDragon);
+        }else{
+            countDragon=1;
+            while (countDragon<1001){
+                if(Math.abs(legS * totalHeads - totalLegs)%countDragon==0){
+                    legsDragon = ((totalLegs - legS * totalHeads)/ countDragon)+legS*headDragon;
+                    if(legsDragon>=0 && totalHeads > countDragon*headDragon){
+                        System.out.print(legsDragon);
+                        break;
+                    }
+                }
+                countDragon++;
+            }
+            if (countDragon == 1001) {
+                System.out.print(-1);
+            }
+        }
+    }
 }
