b65ce4966d189f2c8a7030a9c9ebb7dc75e942ab
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-11-20 23:19:52 +0400

    Degree of Symmetry

--- src/main/java/backend/eolympProblems/problem19/DegreeOfSymmetryApp.java
+++ src/main/java/backend/eolympProblems/problem19/DegreeOfSymmetryApp.java
@@ -1,2 +1,20 @@
-package backend.eolympProblems.problem19;public class DegreeOfSymmetryApp {
+package backend.eolympProblems.problem19;
+
+import java.util.Scanner;
+
+public class DegreeOfSymmetryApp {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String mass = scan.next();
+        int len = mass.length();
+        int count = 0;
+
+        for (int i = 0; i < len / 2 + len % 2; i++) {
+            if (mass.charAt(i) == mass.charAt(len - i - 1)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
